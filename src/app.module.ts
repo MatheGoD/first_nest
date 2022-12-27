c41637fca3117e7b4edf21ab75667da6ba366598
root <root@DESKTOP-L9QJB5R>
2022-12-27 23:15:02 +0900

    시작 준비 완료

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 
+
+//decorator : class에 함수를 추가 할 수 있다. 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [], //엔드포인트 설정 및 함수 실행
+  providers: [],
 })
 export class AppModule {}
